b3134b4b689f69446ab9f1ce8c42b8a444206da2
Gard Rimestad <gard@finn.no>
2012-11-01 09:47:20 +0100

    Fixing new line bug

--- git-prompt/gitprompt.sh
+++ git-prompt/gitprompt.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-if git status 2>&1| grep -q "^# On branch" 
-then
-  BRANCH=`git branch | grep  ^*|awk '{print $2}'`
-  DIRTY=`git status | grep -E "^no changes|^nothing added" > /dev/null && echo "*"`
-  [[ $DIRTY ]] && COLOR="\033[0;31m" || COLOR="\033[0;32m"
-  COLOR_RESET="\033[0m"
+function bash_git_prompt() {
+  if git status 2>&1| grep -q "^# On branch"
+  then
+    BRANCH=`git branch 2>&1 | grep  ^*|awk '{print $2}'`
+    DIRTY=`git status 2>&1 | grep -E "^no changes|^nothing added" > /dev/null && echo "*"`
+    [[ $DIRTY ]] && COLOR="\[\e[0;31m\]" || COLOR="\[\e[0;32m\]"
+    COLOR_RESET="\[\e[m\]"
+    export GITPROMPT="($COLOR$BRANCH$DIRTY$COLOR_RESET)"
+  else 
+    export GITPROMPT=""
+  fi
+}
 
-  echo -e "($COLOR$BRANCH$DIRTY$COLOR_RESET)"
-fi
+#add this to .bashrc
+#bash_prompt() {
+#  . /where/gitprompt/is/installed/gitprompt.sh
+#  bash_git_prompt
+#  PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]${GITPROMPT}\$ "
+#}
 
-
-#add `gitprompt.sh` to $PS1 when gitprompt.sh is in path
+#PROMPT_COMMAND=bash_prompt
